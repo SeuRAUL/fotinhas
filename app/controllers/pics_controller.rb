26ce5e8666ff05cab8a76d731802d9b193f71c9d
seuraul <seuraul.0@gmail.com>
2023-05-09 16:53:08 -0300

    Create new pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,46 @@
 class PicsController < ApplicationController
+  before_action :pic, only: [:edit, :update, :show, :destroy]
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
+
+  def show 
+  end
+
+  def new
+    @pic = Pic.new
+  end
+  
+  def create
+    @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to @pic 
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def pic
+      @pic = Pic.find(params[:id])
+    end
+
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
 end
